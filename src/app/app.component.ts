cf5f986658cf185da2b769e02c60a22d2134e3f1
JulioTrindade <julio.trindade92@gmail.com>
2017-07-10 11:24:40 -0300

    mensagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,16 @@ import {SamuService} from './services/samu.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UFService, SamuService]
 })
 export class AppComponent implements OnInit {
     title = 'app';
     ufs : UF[];
     dados_da_samu : Dados[];
+    municipios_atendidos: Dados[] = [];
+    minha_uf = 26;
+    media: number;
 
     constructor(private ufService: UFService, private samuService: SamuService)
     { }
@@ -22,5 +26,33 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.defineTitle();
+        this.calcularMunicipios();
     }
+
+    defineTitle(): void {
+      for(let uf of this.ufs) {
+        if(uf.id == 26) this.title = uf.nome;
+      }
+    }
+
+    calcularMunicipios(): number{
+      var qtd = 0;
+      var total = 0;
+      for(let mun of this.dados_da_samu){
+        if(mun.uf_id == 26){
+          qtd++;
+          total += mun.valor;
+          this.municipios_atendidos.push(mun)
+        }
+      }
+      return Math.round(total/qtd);
+    }
+
+    defineId(): void {
+      for(let uf of this.ufs) {
+        if(uf.id == 26) this.title = uf.nome;
+      }
+    }
+
 }
